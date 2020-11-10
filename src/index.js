07b19170152d524af1dc75e6f503cc38c566fa00
Requiemu <whudhn@gmail.com>
2020-11-10 16:20:00 +0800

    Update App to use hooks.

--- src/index.js
+++ src/index.js
@@ -248,11 +248,11 @@ function reconcileChildren(parentFiber, elements) {
 
 // ===================== real app ====================
 function Counter() {
-    // const [state, setState] = Didact.useState(1);
+    const [state, setState] = Didact.useState(1);
 
     return (
-        <h1  style="user-select: none">
-            Count: {1}
+        <h1 onClick={() => setState(c => c+1)} style="user-select: none">
+            Count: {state}
         </h1>
     )
 }
